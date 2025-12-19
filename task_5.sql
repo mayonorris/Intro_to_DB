5e0b6fc9f00fa411a18de70c888ed7a67e1e0240
mayonorris <kadanganorris@gmail.com>
2025-12-19 20:42:38 +0000

    insert records

--- task_5.sql
+++ task_5.sql
@@ -3,3 +3,10 @@ USE alx_book_store;
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- task_5.sql
+USE alx_book_store;
+
+-- INSERT INTO customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
