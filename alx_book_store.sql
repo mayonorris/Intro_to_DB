4000659c06017bb06018ec790cf82a538f0bac65
mayonorris <kadanganorris@gmail.com>
2025-12-19 20:14:59 +0000

    alx_book_store database craation

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,48 +4,48 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
--- AUTHORS table
-CREATE TABLE IF NOT EXISTS AUTHORS (
+-- Authors
+CREATE TABLE IF NOT EXISTS Authors (
   author_id INT PRIMARY KEY,
   author_name VARCHAR(215) NOT NULL
 ) ENGINE=INNODB;
 
--- BOOKS table
-CREATE TABLE IF NOT EXISTS BOOKS (
+-- Books
+CREATE TABLE IF NOT EXISTS Books (
   book_id INT PRIMARY KEY,
   title VARCHAR(130) NOT NULL,
   author_id INT NOT NULL,
   price DOUBLE,
   publication_date DATE,
-  CONSTRAINT fk_books_author
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+  CONSTRAINT fk_Books_Author
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 ) ENGINE=INNODB;
 
--- CUSTOMERS table
-CREATE TABLE IF NOT EXISTS CUSTOMERS (
+-- Customers
+CREATE TABLE IF NOT EXISTS Customers (
   customer_id INT PRIMARY KEY,
   customer_name VARCHAR(215) NOT NULL,
   email VARCHAR(215),
   address TEXT
 ) ENGINE=INNODB;
 
--- ORDERS table
-CREATE TABLE IF NOT EXISTS ORDERS (
+-- Orders
+CREATE TABLE IF NOT EXISTS Orders (
   order_id INT PRIMARY KEY,
   customer_id INT NOT NULL,
   order_date DATE,
-  CONSTRAINT fk_orders_customer
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+  CONSTRAINT fk_Orders_Customer
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 ) ENGINE=INNODB;
 
--- ORDER_DETAILS table
-CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
+-- Order_Details
+CREATE TABLE IF NOT EXISTS Order_Details (
   orderdetailid INT PRIMARY KEY,
   order_id INT NOT NULL,
   book_id INT NOT NULL,
   quantity DOUBLE,
-  CONSTRAINT fk_orderdetails_order
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-  CONSTRAINT fk_orderdetails_book
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+  CONSTRAINT fk_OrderDetails_Order
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  CONSTRAINT fk_OrderDetails_Book
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 ) ENGINE=INNODB;
